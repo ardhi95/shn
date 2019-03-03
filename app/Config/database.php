9672997bcd1eae02ab82757feb871ec3489189fa
Ardhi Nur Fauzi <ar.dhi950@gmail.com>
2019-03-03 11:57:30 +0700

    update

---
+++ app/Config/database.php
@@ -0,0 +1,14 @@
+<?php
+class DATABASE_CONFIG {
+	public $default = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'root',
+		'password' => '',
+		'database' => 'uwe2',
+		'prefix' => '',
+		'encoding' => 'utf8',
+	);
+}
+?>
